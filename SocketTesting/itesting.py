c6cfbd221730b42276a41b21c4007c5e46caf9ca
Aditya Uchil <75536724+AdityaU3672@users.noreply.github.com>
2021-06-27 22:06:59 +0530

    Modified byte string to cv2 image conversion

--- SocketTesting/itesting.py
+++ SocketTesting/itesting.py
@@ -8,12 +8,12 @@ img_str = fd.read()
 
 fd.close()
 
-img = cv2.imread(img_str)
+#img = cv2.imread(img_str)
 
-#nparr = np.fromstring(img_str, np.uint8)
-#img_np = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
+nparr = np.frombuffer(img_str, np.uint8)
+img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
 #rgb = np.random.randint(255, size=(900,800,3),dtype=np.uint8)
 
-cv2.imshow('RGB',img)
+cv2.imshow('RGB',img_np)
 cv2.waitKey(0)
