62fc38b9be74a23be49b0540d3a450db430eb289
rspapani <rishipapani@hotmail.com>
2021-07-10 20:54:20 +0530

    almostthere

--- Final/Main2.py
+++ Final/Main2.py
@@ -63,4 +63,5 @@ def loadframe(image):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0',port='5000',debug=True)
+    socketio.run(app, host='127.0.0.1',port='5000',debug=True)
+    #socketio.run(app, host='0.0.0.0',port='5000',debug=True) # For Docker
